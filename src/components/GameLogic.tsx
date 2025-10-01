365451360451f76b3adda1ad74dee213d86b7255
Kiddie22 <wrasula@gmail.com>
2025-10-01 12:24:54 +1000

    Create GameLogic tabs

--- src/components/GameLogic.tsx
+++ src/components/GameLogic.tsx
@@ -1,5 +1,31 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import NewGameTab from "./NewGameTab";
+import { useWeb3 } from "@/contexts/Web3Context";
+import JoinGameTab from "./JoinGameTab";
+
 const GameLogic = () => {
-  return <div>GameLogic</div>;
+  const { walletAddress } = useWeb3();
+
+  return (
+    <div>
+      <span>Welcome {walletAddress}</span>
+
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="new">Start a new game</TabsTrigger>
+          <TabsTrigger value="join">Join a game</TabsTrigger>
+          <TabsTrigger value="results">View previous results</TabsTrigger>
+        </TabsList>
+        <TabsContent value="new">
+          <NewGameTab />
+        </TabsContent>
+        <TabsContent value="join">
+          <JoinGameTab />
+        </TabsContent>
+        <TabsContent value="results">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default GameLogic;
