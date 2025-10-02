e44b021087e2b96862719f8edc6be736b2d6723a
Kiddie22 <wrasula@gmail.com>
2025-10-02 15:43:01 +1000

    Add Toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { Web3Provider } from "./contexts/Web3Context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner";
 
 // Create a tanstack query client
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Web3Provider>
       <QueryClientProvider client={queryClient}>
+        <Toaster />
         <App />
       </QueryClientProvider>
     </Web3Provider>
